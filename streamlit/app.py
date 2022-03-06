f0b60922ed8721280014f09158c8c29c9e6e60e5
Mariella CC <MariellaCC@users.noreply.github.com>
2022-03-06 17:28:56 +0100

    add streamlit page structure

---
+++ streamlit/app.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import streamlit as st
+
+# Custom imports
+from multipage import MultiPage
+from pages import (
+    donnees_preprocessing, exploration_dataviz
+)
+
+app = MultiPage()
+
+# Title of the main page
+st.title("Bookmakerspy - Paris sportifs / Promo FC Juin 2021")
+
+# Add all your application here
+app.add_page("1. Données & pre-processing", donnees_preprocessing.app)
+app.add_page("2. Exploration & dataviz", exploration_dataviz.app)
+
+# The main app
+app.run()
