5d9b2552e5e6cfea953a067665ba5f7afc4dbc2b
ThomasMiras <mirasthomas@gmail.com>
2022-03-06 22:37:24 +0100

    Initial

--- streamlit/app.py
+++ streamlit/app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # Custom imports
 from multipage import MultiPage
 from pages import (
-    donnees_preprocessing, exploration_dataviz
+    donnees_preprocessing, exploration_dataviz, modeles_ML, reduction_dimension, strategie_paris
 )
 
 app = MultiPage()
@@ -17,5 +17,8 @@ app = MultiPage()
 app.add_page("1. Données & pre-processing", donnees_preprocessing.app)
 app.add_page("2. Exploration & dataviz", exploration_dataviz.app)
 
+app.add_page("4. Réduction de dimension", exploration_dataviz.app)
+app.add_page("5. Modèles de Machine Learning", exploration_dataviz.app)
+app.add_page("6. Stratégie de paris", exploration_dataviz.app)
 # The main app
 app.run()
---
+++ streamlit/pages/reduction_dimension.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+
+    st.header("Réduction de dimension")
