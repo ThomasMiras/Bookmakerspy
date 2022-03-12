86178c8d507ea474de0b160a1e61eb54103ada3a
Landry TAYAYA <landry.tayaya@gmail.com>
2022-03-12 15:09:06 +0100

    graphiques buts

--- streamlit/pages/poisson_modelisation.py
+++ streamlit/pages/poisson_modelisation.py
@@ -10,15 +10,26 @@ os.chdir('C:/Users/LANDRYGOL/Documents/GitHub/Bookmakerspy/streamlit')
 sns.set_theme(style="white")
 sns.set_palette("pastel")
 
-df = pd.read_csv('./data/df_results.csv')
-df.rename(columns={'FTHG': 'home_goal', 'FTAG': 'away_goal'}, inplace = 'True')
-df['total_goal'] = df['home_goal'] + df['away_goal']
-
 def app():
 
-    st.header("Bookmakerspy - Simulation par un modèle de Poisson")
+    df = pd.read_csv('./data/df_results.csv', index_col = 0)
+    df.rename(columns={'FTHG': 'home_goal', 'FTAG': 'away_goal'}, inplace = 'True')
+    df['total_goal'] = df['home_goal'] + df['away_goal']
+
+    st.header("Simulation par un modèle de Poisson")
     st.subheader("Rappels sur le nombre de buts par match")
     
-    fig = plt.figure()
-    g = sns.histplot(data = df, x = 'total_goal', bins=10, hue = 'season', kde = True, stat = 'density', multiple = 'dodge');
+    st.dataframe(df)
+    fig, g = plt.figure(figsize=(8, 6))
+    g = sns.histplot(data = df, x = 'total_goal', bins = 10, hue = 'season', kde = True, stat = 'density', multiple = 'dodge');
     g.set_xticks(range(10))
+    st.pyplot(g)
+    
+    
+    #fig, (g1, g2) = plt.subplots(1, 2)
+    #g1 = sns.histplot(data = df, x = 'home_goal', bins = 10, hue = 'season', kde = True, stat = 'density', multiple = 'dodge');
+    #g1.set_xticks(range(10))
+    #st.pyplot(g1)
+    #g2 = sns.histplot(data = df, x = 'away_goal', bins = 10, hue = 'season', kde = True, stat = 'density', multiple = 'dodge');
+    #g2.set_xticks(range(10))
+    #st.pyplot(g2)
