a8d4b83e4c6c3221bf1746dceed87e7e35f63c13
Landry TAYAYA <landry.tayaya@gmail.com>
2022-03-12 13:44:02 +0100

    Update poisson_modelisation.py

--- streamlit/pages/poisson_modelisation.py
+++ streamlit/pages/poisson_modelisation.py
@@ -2,11 +2,20 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import streamlit as st
+import matplotlib.pyplot as plt
 
 sns.set_theme(style="white")
 sns.set_palette("pastel")
+
+df = pd.read_csv('./data/df_results.csv')
+df.rename(columns={'FTHG': 'home_goal', 'FTAG': 'away_goal'}, inplace = 'True')
+df['total_goal'] = df['home_goal'] + df['away_goal']
 
 def app():
 
     st.header("Bookmakerspy - Simulation par un modèle de Poisson")
     st.subheader("Rappels sur le nombre de buts par match")
+    
+    fig = plt.figure()
+    g = sns.histplot(data = df, x = 'total_goal', bins=10, hue = 'season', kde = True, stat = 'density', multiple = 'dodge');
+    g.set_xticks(range(10))
