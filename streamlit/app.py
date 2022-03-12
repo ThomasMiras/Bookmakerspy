f3f94821e294d9bcacc725a7daef3d8b1f9bfba0
Landry TAYAYA <landry.tayaya@gmail.com>
2022-03-12 13:21:05 +0100

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # Custom imports
 from multipage import MultiPage
 from pages import (
-    donnees_preprocessing, exploration_dataviz
+    donnees_preprocessing, exploration_dataviz, poisson_modelisation
 )
 
 app = MultiPage()
@@ -16,6 +16,7 @@ app = MultiPage()
 # Add all your application here
 app.add_page("1. Données & pre-processing", donnees_preprocessing.app)
 app.add_page("2. Exploration & dataviz", exploration_dataviz.app)
+app.add_page("3. Modélisation par un modèle de Poisson", poisson_modelisation.app)
 
 # The main app
 app.run()
