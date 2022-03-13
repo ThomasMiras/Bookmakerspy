3847f33202a2fef18807d3e61af35faa89127299
Landry TAYAYA <landry.tayaya@gmail.com>
2022-03-13 22:12:57 +0100

    probas

--- streamlit/pages/poisson_modelisation.py
+++ streamlit/pages/poisson_modelisation.py
@@ -17,7 +17,7 @@ def app():
     df['total_goal'] = df['home_goal'] + df['away_goal']
 
     st.header('Simulation par un modèle de Poisson')
-    st.subheader('Rappels sur le nombre de buts par match')
+    st.markdown('Rappels sur le nombre de buts par match')
     
     
     season = st.selectbox('season: ', sorted(df['season'].unique()))
@@ -31,6 +31,8 @@ def app():
     st.pyplot(fig)
     
     
+    st.markdown('Les coefficients du modèle sont les suivants:')
+    
     coeffs = pd.DataFrame([[-0.0074, 0.0099, -0.5456, 0.0085, 0.1875, 0.0175, 0.0240, -0.1687, -0.0200, 0.1342],
                           [-0.0033, 0.0101, -2.0334, 0.0279, -0.2725, 0.0350, 0.0446, 0.1306, 0.1001, 0.0300]],
                           index = ["Home", "Away"],
@@ -40,6 +42,8 @@ def app():
     
     st.dataframe(coeffs.style.format("{:20,.4f}"))
     
+    
+    st.markdown('Application de la simulation: match Chelsea - Liverpool du 6 Mai 2018')
     
     features = pd.DataFrame([[19.33333, 7, 0.83112, 56.83333, 6.85429, 14.33333, 9, 6.93083, 7.68778, 1.33333],
                              [17.66667, 4, 0.82617, 63.76667, 6.73786, 10.66667, 4, 6.91444, 7.14444, 1.33333]],
@@ -51,27 +55,32 @@ def app():
     st.dataframe(features.style.format("{:20,.5f}"))
     
     
+    st.markdown('Les valeurs de lambda sont les suivantes:')
+    
     params = pd.DataFrame(index = ["Chelsea", "Liverpool"], columns = pd.Index(['λ'], name = 'Model'))
     params['λ'] = list(np.exp(np.sum(np.multiply(coeffs, features), axis = 1)))
     
     st.dataframe(params.style.format("{:20,.5f}"))
     
     
-    df = pd.DataFrame([['1', '0', '1', '1', '1'], 
-                      ['1', '1', '0', '0', '2'], 
-                      ['D', 'A', 'H', 'H', 'A']], 
-                      index = ["Chelsea", "Liverpool", "Result"],
-                      columns = pd.Index(['Match 1', 'Match 2', 'Match 3', 'Match 4', 'Match 5'], name = 'Model'))
-
-    st.dataframe(df.style.apply(lambda x: ['background-color: #add8e6;' if v =='H' else 'background-color: #90ee90;' if v == 'D' else 'background-color: #ffcccb;' if v == 'A' else 'background-color: #ffffff;' for v in x]))
+    st.markdown('Ce qui donne les tableaux suivants:')
     
-    
+    df = pd.DataFrame(index = ["Chelsea", "Liverpool", "Result"], columns = pd.Index(['Match 1', 'Match 2', 'Match 3', 'Match 4', 'Match 5'], name = 'Model'))
+        
     nb_simu = 10000
     match = {i: pd.DataFrame(columns = range(nb_simu)) for i in ['home', 'away']}
     scorelines = pd.DataFrame(columns = pd.MultiIndex.from_product([['Away'], list(np.arange(9))]), index = pd.MultiIndex.from_product([['Home'], list(np.arange(9))]))
 
     match['home'] = np.random.poisson(params['λ'][0], nb_simu)
     match['away'] = np.random.poisson(params['λ'][1], nb_simu)
+    match['score'] = [str(u) + '-' + str(v) for (u, v) in zip(match['home'], match['away'])]
+    
+    df.loc['Chelsea', :] = [str(e) for e in match['home'][0:5]]
+    df.loc['Liverpool', :] = [str(e) for e in match['away'][0:5]]
+    df.loc['Result', :] = ['H' if u > v else 'D' if u == v else 'A' for (u, v) in zip(df.loc['Chelsea', :], df.loc['Liverpool', :])]
+    
+    st.dataframe(df.style.apply(lambda x: ['background-color: #add8e6;' if v =='H' else 'background-color: #90ee90;' if v == 'D' else 'background-color: #ffcccb;' if v == 'A' else 'background-color: #ffffff;' for v in x]))
+    
 
     for i in range(9):
       for j in range(9):
@@ -97,5 +106,26 @@ def app():
                         color[('Away', j)][('Home', i)] = color[('Away', j)][('Home', i)] + 'border: dashed black;'
         return color
   
-    st.dataframe(scorelines.style.apply(cell_color, axis = None))
-
+    st.dataframe(scorelines.style.apply(cell_color, axis = None).format("{:20,.2f}"))
+    
+    
+    probaH = 0
+    probaD = 0
+    probaA = 0
+    
+    for i in range(9):
+        for j in range(9):
+            if i > j:
+                probaH = probaH + scorelines[('Away', j)][('Home', i)]
+            if i == j:
+                probaD = probaD + scorelines[('Away', j)][('Home', i)]
+            if i < j:
+                probaA = probaA + scorelines[('Away', j)][('Home', i)]
+    
+    probaH = round(probaH, 2)
+    probaD = round(probaD, 2)
+    probaA = round(probaA, 2)
+                
+    st.write('La probabilité pour H est de ', probaH, '%')
+    st.write('La probabilité pour D est de ', probaD, '%') 
+    st.write('La probabilité pour A est de ', probaA, '%') 
