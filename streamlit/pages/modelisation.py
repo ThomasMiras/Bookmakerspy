97a08bc10bc997f6fd8f010228c3ed92dccab77c
Mariella CC <MariellaCC@users.noreply.github.com>
2022-03-13 23:27:46 +0100

    improve layout slide 5

--- streamlit/pages/modelisation.py
+++ streamlit/pages/modelisation.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
+from sklearn import neighbors
 from sklearn import linear_model
 from sklearn.model_selection import GridSearchCV, cross_val_score   
 from sklearn.decomposition import PCA
@@ -15,6 +17,8 @@ def app():
     st.header("Bookmakerspy - Modélisation")
 
     st.subheader("Comparaison de modèles")
+
+    colors = sns.color_palette('pastel')
     
     # fonction qui créée les train et test
     @st.cache
@@ -75,29 +79,16 @@ def app():
     # fonction qui créée les dashboards
     def create_dashboard(metricsNR,metricsFS,metricsR):
 
-        st.caption('Dataset non réduit')
+        
+        col1,col2,col3,col4,col5 = st.columns([1,.2,1,.2,1])
 
-        col1,col2,col3 = st.columns([1,.5,2])
-
-        f1_score_nr = [metricsNR[3]['A']['f1-score'],metricsNR[3]['D']['f1-score'],metricsNR[3]['H']['f1-score']]
-        conf_matrix_nr = confusion_matrix(y_test, metricsNR[2])
         
         with col1:
-            st.write("F1 Score")
-            fig, ax = plt.subplots()
-            ax.bar(x=['Away','Draw','Home'],height=f1_score_nr)
-            ax.spines['right'].set_visible(False)
-            ax.spines['top'].set_visible(False) 
-            plt.yticks(fontsize=16)
-            plt.xticks(fontsize=16)
-            st.pyplot(fig)
-
-            st.metric(label="Accuracy Train", value=round(metricsNR[4], 2))
-            st.metric(label="Accuracy Test", value=round(metricsNR[5], 2))
+            st.write('Dataset non réduit')
             
+            f1_score_nr = [metricsNR[3]['A']['f1-score'],metricsNR[3]['D']['f1-score'],metricsNR[3]['H']['f1-score']]
+            conf_matrix_nr = confusion_matrix(y_test, metricsNR[2])
 
-        with col3:
-            st.write("Matrice de confusion")
             fig, ax = plt.subplots()
             ax.matshow(conf_matrix_nr, cmap=plt.cm.Blues, alpha=0.3)
             for i in range(conf_matrix_nr.shape[0]):
@@ -113,6 +104,121 @@ def app():
             plt.xlabel('Predicted', fontsize=16)
             plt.ylabel('Real', fontsize=16)
             st.pyplot(fig)
+
+            d = {'Accuracy Train': [round(metricsNR[4], 2)], 'Accuracy Test': [round(metricsNR[5], 2)]}
+            
+            hide_table_row_index = """
+            <style>
+            tbody th {display:none}
+            .blank {display:none}
+            </style>
+            """
+            st.markdown(hide_table_row_index, unsafe_allow_html=True)
+            st.table(pd.DataFrame(d).style.format("{:.2}"))
+
+            st.caption("F1 Score")
+            fig, ax = plt.subplots()
+            
+            ax.bar(x=['Away','Draw','Home'],height=f1_score_nr,color = [colors[0],colors[2],colors[1]])
+            ax.spines['right'].set_visible(False)
+            ax.spines['top'].set_visible(False) 
+            plt.yticks(fontsize=16)
+            plt.xticks(fontsize=16)
+            st.pyplot(fig)
+
+            
+        with col3:
+            
+            st.write('Dataset sélection de features')
+
+            f1_score_fs = [metricsFS[3]['A']['f1-score'],metricsFS[3]['D']['f1-score'],metricsFS[3]['H']['f1-score']]
+            conf_matrix_fs = confusion_matrix(y_test, metricsFS[2])
+            
+
+            fig, ax = plt.subplots()
+            ax.matshow(conf_matrix_fs, cmap=plt.cm.Blues, alpha=0.3)
+            for i in range(conf_matrix_fs.shape[0]):
+                for j in range(conf_matrix_fs.shape[1]):
+                    ax.text(x=j,y=i,s=conf_matrix_fs[i, j], va='center', ha='center',fontsize=18)
+            
+            labels = ['Away','Draw','Home']
+            ax.set_xticklabels(['']+labels)
+            ax.set_yticklabels(['']+labels)
+            plt.xticks(fontsize=14)
+            plt.yticks(fontsize=14)
+            ax.xaxis.set_label_position('top')
+            plt.xlabel('Predicted', fontsize=16)
+            plt.ylabel('Real', fontsize=16)
+            st.pyplot(fig)
+
+            d = {'Accuracy Train': [round(metricsFS[4], 2)], 'Accuracy Test': [round(metricsFS[5], 2)]}
+            
+            hide_table_row_index = """
+            <style>
+            tbody th {display:none}
+            .blank {display:none}
+            </style>
+            """
+            st.markdown(hide_table_row_index, unsafe_allow_html=True)
+            st.table(pd.DataFrame(d).style.format("{:.2}"))
+
+            
+            st.caption("F1 Score")
+            fig, ax = plt.subplots()
+            ax.bar(x=['Away','Draw','Home'],height=f1_score_fs,color = [colors[0],colors[2],colors[1]])
+            ax.spines['right'].set_visible(False)
+            ax.spines['top'].set_visible(False) 
+            plt.yticks(fontsize=16)
+            plt.xticks(fontsize=16)
+            st.pyplot(fig)
+        
+        with col5:
+        
+            st.write('Dataset réduit')
+
+            f1_score_r = [metricsR[3]['A']['f1-score'],metricsR[3]['D']['f1-score'],metricsR[3]['H']['f1-score']]
+            conf_matrix_r = confusion_matrix(y_test, metricsR[2])
+
+                
+            fig, ax = plt.subplots()
+            ax.matshow(conf_matrix_r, cmap=plt.cm.Blues, alpha=0.3)
+            
+            for i in range(conf_matrix_r.shape[0]):
+                for j in range(conf_matrix_r.shape[1]):
+                    ax.text(x=j,y=i,s=conf_matrix_r[i, j], va='center', ha='center',fontsize=18)
+            
+            labels = ['Away','Draw','Home']
+            ax.set_xticklabels(['']+labels)
+            ax.set_yticklabels(['']+labels)
+            plt.xticks(fontsize=14)
+            plt.yticks(fontsize=14)
+            ax.xaxis.set_label_position('top')
+            plt.xlabel('Predicted', fontsize=16)
+            plt.ylabel('Real', fontsize=16)
+            st.pyplot(fig)
+
+            d = {'Accuracy Train': [round(metricsR[4], 2)], 'Accuracy Test': [round(metricsR[5], 2)]}
+            
+            hide_table_row_index = """
+            <style>
+            tbody th {display:none}
+            .blank {display:none}
+            </style>
+            """
+            st.markdown(hide_table_row_index, unsafe_allow_html=True)
+            st.table(pd.DataFrame(d).style.format("{:.2}"))   
+
+            st.caption("F1 Score")
+            fig, ax = plt.subplots()
+            ax.bar(x=['Away','Draw','Home'],height=f1_score_r,color = [colors[0],colors[2],colors[1]])
+            ax.spines['right'].set_visible(False)
+            ax.spines['top'].set_visible(False) 
+            plt.yticks(fontsize=16)
+            plt.xticks(fontsize=16)
+            st.pyplot(fig)
+            
+
+            
 
     
     # création des datasets train et test
@@ -164,10 +270,45 @@ def app():
         
         return [[clf_NR,grid_clf_NR,y_pred_cfl_NR,report_NR,accuracy_train_NR,accuracy_test_NR],[clf_FS,grid_clf_FS,y_pred_cfl_FS,report_FS,accuracy_train_FS,accuracy_test_FS],[clf_R,grid_clf_R,y_pred_cfl_R,report_R,accuracy_train_R,accuracy_test_R]]
 
-    
+    # création des modèles de K plus proche voisins
+    @st.cache
+    def get_data_knn():
+        parametres = {'n_neighbors': [10, 20, 30, 40, 50], 'metric': ['manhattan', 'chebyshev', 'minkowski', 'l1' , 'l2'] }
+
+        knn_NR = neighbors.KNeighborsClassifier()
+        knn_FS = neighbors.KNeighborsClassifier()
+        knn_R  = neighbors.KNeighborsClassifier()
+
+        grid_knn_NR = GridSearchCV(estimator=knn_NR, param_grid=parametres)
+        grid_knn_FS = GridSearchCV(estimator=knn_FS, param_grid=parametres)
+        grid_knn_R = GridSearchCV(estimator=knn_R, param_grid=parametres)
+
+        grid_knn_NR.fit(X_train_NR,y_train)
+        grid_knn_FS.fit(X_train_FS,y_train)
+        grid_knn_R.fit(X_train_R,y_train)
+
+        y_pred_knn_NR = grid_knn_NR.predict(X_test_NR)
+        y_pred_knn_FS = grid_knn_FS.predict(X_test_FS)
+        y_pred_knn_R  = grid_knn_R.predict(X_test_R)
+        
+        report_NR = classification_report(y_test, pd.DataFrame(y_pred_knn_NR),output_dict=True)
+        accuracy_train_NR = grid_knn_NR.score(X_train_NR, y_train)
+        accuracy_test_NR = grid_knn_NR.score(X_test_NR, y_test)
+
+        report_FS = classification_report(y_test, pd.DataFrame(y_pred_knn_FS),output_dict=True)
+        accuracy_train_FS = grid_knn_FS.score(X_train_FS, y_train)
+        accuracy_test_FS = grid_knn_FS.score(X_test_FS, y_test)
+
+        report_R = classification_report(y_test, pd.DataFrame(y_pred_knn_R),output_dict=True)
+        accuracy_train_R = grid_knn_R.score(X_train_R, y_train)
+        accuracy_test_R = grid_knn_R.score(X_test_R, y_test)
+        
+        return [[knn_NR,grid_knn_NR,y_pred_knn_NR,report_NR,accuracy_train_NR,accuracy_test_NR],[knn_FS,grid_knn_FS,y_pred_knn_FS,report_FS,accuracy_train_FS,accuracy_test_FS],[knn_R,grid_knn_R,y_pred_knn_R,report_R,accuracy_train_R,accuracy_test_R]]
+
+
     option = st.selectbox(
      'Choisir un modèle',
-     ('Régression logistique', 'Home phone', 'Mobile phone'))
+     ('Régression logistique', 'K plus proches voisins', 'Mobile phone'))
 
     if option == 'Régression logistique':
 
@@ -178,11 +319,20 @@ def app():
             logregR = get_data_logreg()[2]
 
             create_dashboard(logregNR,logregFS,logregR)
+    
+    if option == 'K plus proches voisins':
+
+        with st.spinner('Calculs en cours'):
+           
+            knnNR = get_data_knn()[0]
+            knnFS = get_data_knn()[1]
+            knnR = get_data_knn()[2]
+
+            create_dashboard(knnNR,knnFS,knnR)
+
+ 
 
         
-
-
-
 
     #print(classification_report(y_test, pd.DataFrame(logregNR[2])))
 
