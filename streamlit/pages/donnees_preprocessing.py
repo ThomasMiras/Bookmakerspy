d9d44b5699940a1bac3d690092940c9dd276b11e
Mariella CC <MariellaCC@users.noreply.github.com>
2022-03-21 11:29:12 +0100

    amelioration img et ajout ponctuation

--- streamlit/pages/donnees_preprocessing.py
+++ streamlit/pages/donnees_preprocessing.py
@@ -9,58 +9,58 @@ def app():
     st.subheader("Introduction")
 
     st.markdown("* Contexte")
-    st.write("Football - English Premier League 2014/15 - 2017/18")
-    st.write("Paris sportifs: mise en place d'une stratégie pour tenter de battre les bookmakers")
+    st.write("Football - English Premier League 2014/15 - 2017/18.")
+    st.write("Paris sportifs: mise en place d'une stratégie pour tenter de battre les bookmakers.")
 
     st.markdown("* Origine des données")
-    st.write("Kaggle: données concernant les matchs et les joueurs")
-    st.write("Datahub: données concernant les cotes")
+    st.write("Kaggle: données concernant les matchs et les joueurs.")
+    st.write("Datahub: données concernant les cotes.")
 
     st.subheader("Collecte des données")
 
 
     st.markdown("* Données équipes et joueurs")
 
-    st.caption("Données JSON")
+    st.caption("-&nbsp;&nbsp;Données JSON")
     f = json.load(open('./data/season_stats_min.json'))
     st.json(f['1190174'])
 
-    st.caption("Dataframe équipes")
+    st.caption("-&nbsp;&nbsp;Dataframe équipes")
     
     df_home = pd.read_csv('./data/df_home.csv')
     st.dataframe(df_home)
 
-    st.caption("Dataframe joueurs")
+    st.caption("-&nbsp;&nbsp;Dataframe joueurs")
     df_position_home_rating = pd.read_csv('./data/df_position_home_rating.csv')
     st.dataframe(df_position_home_rating)
 
-    st.caption("Dataframe cotes")
+    st.caption("-&nbsp;&nbsp;Dataframe cotes")
     df_odds = pd.read_csv('./data/df_odds.csv')
     st.dataframe(df_odds)
 
     st.subheader("Pre-processing")
     
-    st.caption("Jointures")
-    img_jointures = Image.open('./data/img/jointures_df.png')
+    st.caption("-&nbsp;&nbsp;Jointures")
+    img_jointures = Image.open('./data/img/jointures_df_n.png')
     st.image(img_jointures)
 
-    st.caption("Traitement des valeurs manquantes")
-    st.write("Données de match: valeurs manquantes considérées comme absence d'événement (0)")
+    st.caption("-&nbsp;&nbsp;Traitement des valeurs manquantes")
+    st.write("Données de match: valeurs manquantes considérées comme absence d'événement (0).")
     st.write("Données joueurs: les joueurs non notés (0) n'ont pas été pris en compte pour calculer les moyennes par position.")
 
-    st.caption("Suppression de variables")
-    img_supp = Image.open('./data/img/suppression_variables.png')
+    st.caption("-&nbsp;&nbsp;Suppression de variables")
+    img_supp = Image.open('./data/img/suppression_variables_n.png')
     st.image(img_supp)
 
-    st.caption("Regroupements de variables")
-    st.write("home/away pass: ratio entre home/away accurate pass et home/away total pass")
+    st.caption("-&nbsp;&nbsp;Regroupements de variables")
+    st.write("Home/away pass: ratio entre home/away accurate pass et home/away total pass")
     
-    st.caption("Création de variables")
-    st.write("Différences entre les notes joueurs home et away")
-    st.write("Différences entre le nombre de goals home et away")
+    st.caption("-&nbsp;&nbsp;Création de variables")
+    st.write("Différences entre les notes joueurs home et away.")
+    st.write("Différences entre le nombre de goals home et away.")
 
-    st.caption("Moyennes des 3 derniers matchs")
-    st.write("Moyennes effectuées sur les stats pour chaque équipe sur les 3 derniers matchs où une équipe a joué dans la même situation (home/away)")
+    st.caption("-&nbsp;&nbsp;Moyennes des 3 derniers matchs")
+    st.write("Moyennes effectuées sur les stats pour chaque équipe sur les 3 derniers matchs où une équipe a joué dans la même situation (home/away).")
 
 
 
